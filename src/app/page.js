46a4a4ad0f9129884255ff48b7c5309e30baacc5
Abhirup Saha <179203311+ConsoleCzar-2@users.noreply.github.com>
2025-08-26 20:48:49 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,19 @@
 import Link from "next/link";
 
+import VisitorTracker from '../components/VisitorTracker';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <VisitorTracker />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
+
+
 export default function Home() {
   return (
     <div className="block">
