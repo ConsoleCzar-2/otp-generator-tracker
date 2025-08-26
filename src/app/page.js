1f8455fcd93dcd20b96113f92e77656a7c9d84c1
Abhirup Saha <179203311+ConsoleCzar-2@users.noreply.github.com>
2025-08-26 21:18:10 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -27,25 +27,3 @@ export default function Home() {
   );
 }
 
-
-export default function Home() {
-  return (
-    <div className="block">
-      <h1 className="otp fade-in">OTP System</h1>
-      <nav>
-        <ul>
-          <div className="links fade-in">
-          <li>
-            <Link href="/generator"><span className="front">OTP Generator</span></Link>
-          </li>
-          </div>
-          <div className="links fade-in">
-          <li>
-            <Link href="/receiver"><span className="front">OTP Receiver</span></Link>
-          </li>
-          </div>
-        </ul>
-      </nav>
-    </div>
-  );
-}
