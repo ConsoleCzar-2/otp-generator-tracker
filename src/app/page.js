73772681d7d20d77a9e367e61e15bd5275e85513
Abhirup Saha <179203311+ConsoleCzar-2@users.noreply.github.com>
2025-08-26 22:24:58 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -3,29 +3,31 @@ import VisitorTracker from './components/VisitorTracker';
 
 export default function Home() {
   return (
-    <div className="block">
-      <VisitorTracker />
-      {
-        <div>
-          <h1 className="otp fade-in">OTP System</h1>
-          <p>Generate secure OTP codes</p>
-        </div>
-      }
-      <nav>
-        <ul>
-          <div className="links fade-in">
-          <li>
-            <Link href="/generator"><span className="front">OTP Generator</span></Link>
-          </li>
+    <>
+      <div className="block">
+        <VisitorTracker />
+        {
+          <div>
+            <h1 className="otp fade-in">OTP System</h1>
+            <p>Generate secure OTP codes</p>
           </div>
-          <div className="links fade-in">
-          <li>
-            <Link href="/receiver"><span className="front">OTP Receiver</span></Link>
-          </li>
-          </div>
-        </ul>
-      </nav>
-    </div>
+        }
+        <nav>
+          <ul>
+            <div className="links fade-in">
+            <li>
+              <Link href="/generator"><span className="front">OTP Generator</span></Link>
+            </li>
+            </div>
+            <div className="links fade-in">
+            <li>
+              <Link href="/receiver"><span className="front">OTP Receiver</span></Link>
+            </li>
+            </div>
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 }
 
