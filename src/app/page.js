04ce0ed3fd883c5bddcf092b8e75d175d2f61e42
Abhirup Saha <179203311+ConsoleCzar-2@users.noreply.github.com>
2025-08-26 22:16:28 +0530

    visuals

--- src/app/page.js
+++ src/app/page.js
@@ -7,22 +7,24 @@ export default function Home() {
       <VisitorTracker />
       {
         <div>
-          <h1>OTP Generator</h1>
+          <h1 className="otp fade-in">OTP System</h1>
           <p>Generate secure OTP codes</p>
         </div>
       }
-      <div>
-        {
-          <ul>
-            <li>
-              <Link href="/generator">OTP Generator</Link>
-            </li>
-            <li>
-              <Link href="/receiver">OTP Receiver</Link>
-            </li>
-          </ul>
-        }
-      </div>
+      <nav>
+        <ul>
+          <div className="links fade-in">
+          <li>
+            <Link href="/generator"><span className="front">OTP Generator</span></Link>
+          </li>
+          </div>
+          <div className="links fade-in">
+          <li>
+            <Link href="/receiver"><span className="front">OTP Receiver</span></Link>
+          </li>
+          </div>
+        </ul>
+      </nav>
     </>
   );
 }
