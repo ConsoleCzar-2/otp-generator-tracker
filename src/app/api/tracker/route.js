70244a24a3cfe60f9e2bb8e85062d34d5af97f3f
Abhirup Saha <179203311+ConsoleCzar-2@users.noreply.github.com>
2025-08-26 22:42:31 +0530

    new

--- src/app/api/tracker/route.js
+++ src/app/api/tracker/route.js
@@ -4,8 +4,6 @@ export async function POST(request) {
     request.headers.get("x-real-ip") ||
     undefined;
 
-  console.log("VisitorTracker API called. IP:", ip);
-
   const supabaseUrl = process.env.SUPABASE_URL;
   const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
@@ -18,10 +16,6 @@ export async function POST(request) {
     },
     body: JSON.stringify({ ip_address: ip }),
   });
-
-  console.log("Website response status:", response.status);
-  const responseBody = await response.text();
-  console.log("Website response body:", responseBody);
 
   return new Response(JSON.stringify({ message: "IP stored", ip }), {
     status: 200,
